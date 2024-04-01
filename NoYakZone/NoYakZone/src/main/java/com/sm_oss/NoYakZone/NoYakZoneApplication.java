475e7e440acc94a1ae23caeb39641858324c050a
nahollo <skgudwlsdlrj@gmail.com>
2024-04-01 14:03:08 +0900

    Na_first_commit
    add springboot

---
+++ NoYakZone/NoYakZone/src/main/java/com/sm_oss/NoYakZone/NoYakZoneApplication.java
@@ -0,0 +1,13 @@
+package com.sm_oss.NoYakZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoYakZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoYakZoneApplication.class, args);
+	}
+
+}
---
+++ NoYakZone/NoYakZone/src/test/java/com/sm_oss/NoYakZone/NoYakZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm_oss.NoYakZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoYakZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
