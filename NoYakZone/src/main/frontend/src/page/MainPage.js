35f9738503a249c43259a768390efec4bfce3171
송동준 <dongjun569@naver.com>
2024-05-28 16:42:40 +0900

    약품소개페이지
    검색 + 약학정보원 페이지 링크

--- NoYakZone/src/main/frontend/src/page/MainPage.js
+++ NoYakZone/src/main/frontend/src/page/MainPage.js
@@ -2,21 +2,27 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import { useHistory } from 'react-router-dom';
-import '../CSS/MainPage.css'
+import '../CSS/MainPage.css';
 
+import poster1 from '../image/마약포스터1.jpg';
+import poster2 from '../image/마약포스터2.png';
 
 const MainPage = () => {
     const history = useHistory();
 
     const goToAboutPage = () => {
-        history.push('/about');
+        history.push('/About');
     };
 
-    const goToDrugInfo = () =>(
-        history.push('/Community')
-    )
+    const goToDrugInfo = () => {
+        history.push('/Community');
+    };
+
+    const goToChatBot = () => {
+        history.push('/ChatBot');
+    };
 
     return (
         <div className='MainContainer'>
@@ -30,10 +36,28 @@ const MainPage = () => {
                         AutoPlay도 가능하면 쓸수있게
                     </p>
 
-                    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                        <SwiperSlide>Slide 1</SwiperSlide>
-                        <SwiperSlide>Slide 2</SwiperSlide>
-                        <SwiperSlide>Slide 3</SwiperSlide>
+                    <Swiper 
+                        navigation={true} 
+                        pagination={{ clickable: true }}
+                        modules={[Navigation, Pagination]} 
+                        className="mySwiper"
+                        autoplay={{ delay: 3000, disableOnInteraction: false }}
+                    >
+                        <SwiperSlide>
+                            <div className="con">
+                                <div className="txtwrap">
+                                    <h3>Making Tomorrow Better</h3>
+                                    <p>끊임없는 신약 연구개발과 우수의약품 생산을 위해 정진하여 인류의 건강한 미래를 추구합니다.</p>
+                                </div>
+                                <div className="ctr">
+                                    <div className="vprev" tabIndex="0" role="button" aria-label="Previous slide"></div>
+                                    <div className="swiper-pagination vpaging swiper-pagination-clickable swiper-pagination-bullets"></div>
+                                    <div className="vnext" tabIndex="0" role="button" aria-label="Next slide"></div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide><img src={poster1} alt="Poster 1" /></SwiperSlide>
+                        <SwiperSlide><img src={poster2} alt="Poster 2" /></SwiperSlide>
                     </Swiper>
                 </div>
 
@@ -41,8 +65,13 @@ const MainPage = () => {
                     <h2>약 소개 페이지</h2>
                 </div>
 
-                <div className='DrugInfo' onClick={goToAboutPage}>
-                    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                <div className='DrugInfo'>
+                    <Swiper 
+                        navigation={true} 
+                        pagination={{ clickable: true }}
+                        modules={[Navigation, Pagination]} 
+                        className="mySwiper"
+                    >
                         <SwiperSlide>Slide 1</SwiperSlide>
                         <SwiperSlide>Slide 2</SwiperSlide>
                         <SwiperSlide>Slide 3</SwiperSlide>
@@ -55,16 +84,14 @@ const MainPage = () => {
                     </Swiper>
                 </div>
 
-                <div className='Community'onClick={goToDrugInfo}>
+                <div className='Community' onClick={goToDrugInfo}>
                     <h2>커뮤니티</h2>
                 </div>
 
-                <div className='ChatBot'>
+                <div className='ChatBot' onClick={goToChatBot}>
                     <p>상담 챗봇 페이지</p>
                 </div>
             </div>
-
-
         </div>
     );
 };
