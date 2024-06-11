1b3980e86f4098cc7f45ca9397b7c0de044b494e
송동준 <dongjun569@naver.com>
2024-06-11 16:16:13 +0900

    디자인중

--- NoYakZone/src/main/frontend/src/page/MainPage.js
+++ NoYakZone/src/main/frontend/src/page/MainPage.js
@@ -12,6 +12,22 @@ import poster3 from '../image/마약포스터3.png';
 import poster4 from '../image/마약포스터4.png';
 import poster5 from '../image/마약포스터5.png';
 
+//마약 데이터
+import fentanyl from '../image/펜타닐.png'; 
+import ketamine from '../image/케타민.png';
+import cocaine from '../image/코카인.png';
+import methylphenidate from '../image/페니드.png';
+import noImage from '../image/이미지 없음.png';
+
+const exampleData = [
+    { id: 1, name: '펜타닐(Fentanyl)', imageUrl: fentanyl, link: 'http://www.health.kr/Menu.PharmReview/_uploadfiles/펜타닐(fentanyl).pdf' },
+    { id: 2, name: '케타민(Ketamine)', imageUrl: ketamine, link: 'http://www.health.kr/Menu.PharmReview/_uploadfiles/케타민(ketamine).pdf' },
+    { id: 3, name: '코카인(Cocaine)', imageUrl: cocaine, link: 'https://www.msdmanuals.com/ko-kr/home/특별-주제/불법-약물-및-중독성-물질/코카인' },
+    { id: 4, name: '메틸페니데이트(Methylphenidate)', imageUrl: methylphenidate, link: 'https://www.health.kr/Menu.PharmReview/_uploadfiles/메틸페니데이트.pdf' },
+    { id: 5, name: '디히드로코데인(Dihydrocodeine)', imageUrl: noImage, link: 'https://www.health.kr/Menu.PharmReview/View.asp?PharmReview_IDX=8677' },
+    { id: 6, name: '대마초(Cannabis)', imageUrl: noImage, link: 'https://www.health.kr/Menu.PharmReview/View.asp?PharmReview_IDX=8609' },
+];
+
 const MainPage = () => {
     const history = useHistory();
 
@@ -27,16 +43,19 @@ const MainPage = () => {
         history.push('/ChatBot');
     };
 
+    const handleSlideClick = (link) => {
+        window.open(link, '_blank');
+    };
+
     return (
         <div className='MainContainer'>
             <div className='Content'>
                 <div className='ServiceInfo'>
                     <h2>서비스 소개</h2>
-                    <p className='serviceGoal'>
+                    <p className='serviceGoal' onClick={goToAboutPage}>
                         우리의 사명은 마약 사용을 줄이고, 이를 통해 사회의 안전과 건강을 증진하는 것입니다.
-                    </p>
-                    <p className='autoPlay'>
-                        다양한 대상들에게 맞춤형 서비스를 제공하여 이 목표를 달성하고자 합니다.
+                        <p>다양한 대상들에게 맞춤형 서비스를 제공하여 이 목표를 달성하고자 합니다.
+                        </p>
                     </p>
 
                     <div className="slideContent">
@@ -64,22 +83,22 @@ const MainPage = () => {
                     <h2>약 소개 페이지</h2>
                 </div>
 
-                <div className='DrugInfo'>
+                <div className='DrugInfo drugInfoContent'>
                     <Swiper 
                         navigation={true} 
                         pagination={{ clickable: true }}
-                        modules={[Navigation, Pagination]} 
+                        modules={[Navigation, Pagination, Autoplay]} 
                         className="mySwiper"
+                        autoplay={{ delay: 3000, disableOnInteraction: false }}
                     >
-                        <SwiperSlide>Slide 1</SwiperSlide>
-                        <SwiperSlide>Slide 2</SwiperSlide>
-                        <SwiperSlide>Slide 3</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 5</SwiperSlide>
-                        <SwiperSlide>Slide 6</SwiperSlide>
-                        <SwiperSlide>Slide 7</SwiperSlide>
-                        <SwiperSlide>Slide 8</SwiperSlide>
-                        <SwiperSlide>Slide 9</SwiperSlide>
+                        {exampleData.slice(0, 6).map((item) => (
+                            <SwiperSlide key={item.id} onClick={() => handleSlideClick(item.link)}>
+                                <div>
+                                    <img src={item.imageUrl} alt={item.name} className="poster-image" />
+                                    <div className="slideTitle">{item.name}</div>
+                                </div>
+                            </SwiperSlide>
+                        ))}
                     </Swiper>
                 </div>
 
